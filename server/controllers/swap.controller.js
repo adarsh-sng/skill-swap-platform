fe784457ab1815d243e98f00a5f60c1afc50aed5
adarsh-sng <adarshishu79@gmail.com>
2025-07-12 12:50:02 +0530

    backend(logic): add functionality for seeing sent and received swaps.

--- server/controllers/swap.controller.js
+++ server/controllers/swap.controller.js
@@ -17,3 +17,28 @@ export const createSwap = async (req, res) => {
     res.status(500).json({ message: 'Error creating swap request' });
   }
 };
+
+export const getSentSwaps = async (req, res) => {
+  try {
+    const swaps = await Swap.find({ fromUser: req.user.id })
+      .populate('toUser', 'name skillsOffered skillsWanted') // return basic info
+      .sort({ createdAt: -1 });
+
+    res.json(swaps);
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching sent swaps' });
+  }
+};
+
+export const getReceivedSwaps = async (req, res) => {
+  try {
+    const swaps = await Swap.find({ toUser: req.user.id })
+      .populate('fromUser', 'name skillsOffered skillsWanted')
+      .sort({ createdAt: -1 });
+
+    res.json(swaps);
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching received swaps' });
+  }
+};
+
--- server/routes/swap.routes.js
+++ server/routes/swap.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { createSwap } from '../controllers/swap.controller.js';
+import { createSwap, getReceivedSwaps, getSentSwaps } from '../controllers/swap.controller.js';
 
 const router = express.Router();
 
 router.post('/', protect, createSwap);
+router.get('/sent', protect, getSentSwaps);
+router.get('/received', protect, getReceivedSwaps);
 
 export default router;
